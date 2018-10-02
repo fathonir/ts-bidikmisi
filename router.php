36ce97e7179cb128a7a54ddbcc30ae12fa9f0356
Fathoni <m.fathoni@mail.com>
2018-10-02 19:57:13 +0700

    Dibuat agar bisa mengenali file index.html jika tidak ada index.php

--- router.php
+++ router.php
@@ -9,8 +9,13 @@
 if (file_exists(__DIR__ . '/' . $_SERVER['REQUEST_URI'])) {
     return false; // serve the requested resource as-is.
 } else {
-    // this is the important part!
-    $_SERVER['SCRIPT_NAME'] = '/index.php';
-    
-    include_once (__DIR__ . '/index.php');
+	if (file_exists(__DIR__ . '/index.html')) {
+		$_SERVER['SCRIPT_NAME'] = '/index.html';
+	}
+	else {
+		// this is the important part!
+		$_SERVER['SCRIPT_NAME'] = '/index.php';
+
+		include_once (__DIR__ . '/index.php');
+	}
 }
